933169b01b13e794da88c4e30d1976c8e15a3967
sephhsephh <153965319+sephhsephh@users.noreply.github.com>
2024-03-15 13:25:27 +0800

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
         <header>
             <div className="box abtBox">
                 <span></span>
-                <h1>Good Day! I am <span>Joseph Estilo</span></h1>
+                <h1>Good Day! I am <span>Carl Timtiman</span></h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae tortor vitae diam interdum pharetra. Pellentesque elementum sem augue, ut suscipit mauris fringilla id. Praesent condimentum nisl quis lorem dignissim, eu volutpat nulla tristique. Quisque sed urna sit amet eros viverra imperdiet vitae imperdiet tellus. Cras ultrices fringilla ornare. Cras quis nulla dictum, vestibulum nunc at, tincidunt arcu. Aenean maximus turpis mauris, at posuere turpis gravida tincidunt.</p>
             </div>
             <div className="box imgBox">
